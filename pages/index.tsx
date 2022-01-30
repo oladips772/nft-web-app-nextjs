7e2b3affd5e93a007ac8817db198512b3071a1ab
oladips <oladips200@gmail.com>
2022-01-30 15:57:12 +0000

    created nft collections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <title>NFT web-app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-red-900">sanity nft </h1>
+      <Header />
     </div>
   )
 }
