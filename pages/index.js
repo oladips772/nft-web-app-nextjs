fdc78e7e61c71ddd9897c0c8154a9db731558c52
oladips <oladips200@gmail.com>
2022-01-31 05:43:58 +0000

    created a an optional login div

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import { useWeb3 } from '@3rdweb/hooks'
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
+
+export default function Home() {
+  const { address, connectWallet } = useWeb3()
+
+  return (
+    <div className={style.wrapper}>
+      <Head>
+        <title>NFT web-app</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+          <div className={style.walletConnectWrapper}>
+            <button className={style.button}>Connect wallet </button>
+            <div className={style.details}>You need chrome to be able to run this app</div>
+          </div>
+      )}
+    </div>
+  )
+}
